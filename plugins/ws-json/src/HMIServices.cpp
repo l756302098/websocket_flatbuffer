c9e1fd113c5c537ffc6ee20e0c01562987040ef3
liyanlong <liyanlong@indemind.cn>
2023-08-09 17:07:59 +0800

    mod base64

--- plugins/ws-json/src/HMIServices.cpp
+++ plugins/ws-json/src/HMIServices.cpp
@@ -50,39 +50,41 @@ int main(int argc,char** argv){
     wf.Accept([](bool connected){
         std::cout << "connected:" << connected <<  std::endl;
     });
-    std::vector<std::uint8_t> imagebuf;
-    cv::Mat image = cv::imread("/home/li/test.png", cv::IMREAD_UNCHANGED);
-    if (!cv::imencode(".png", image, imagebuf)) {
-        std::cout << "fail to imencode(.png). w:" << image.cols
-                    << ", h:" << image.rows << std::endl;
-        return 1;
-    }
+    
+    cv::Mat mat = cv::imread("/home/li/test.png", cv::IMREAD_UNCHANGED);
+
+    int params[3] = {0};
+	params[0] = CV_IMWRITE_JPEG_QUALITY;
+	params[1] = 100;
+
+	std::vector<uchar> imageBuf;
+	bool code = cv::imencode(".jpg", mat,imageBuf, std::vector<int>(params, params+2));
+
     wf.Start();
     sleep(10);
     // self-test
     int count = 0;
-    while (++count<11)
+    while (++count<52)
     {
-        std::string encode_str = base64_encode(imagebuf.data(),imagebuf.size());
-        std::cout << "image size:" << imagebuf.size() 
-            << " encode size:" << encode_str.size() << std::endl;
+        std::string encode_str = base64_encode(&imageBuf[0],imageBuf.size());
+        //std::cout  << " encode size:" << encode_str.size() << std::endl;
+        std::cout  << "encode str:" << encode_str << std::endl;
         nlohmann::json ijson;
         ijson["data"] = encode_str;
         ijson["encoding"] = "base64";
-        ijson["width"] = image.cols;
-        ijson["height"] = image.rows;
+        ijson["width"] = mat.cols;
+        ijson["height"] = mat.rows;
         wf.SendReport(ijson);
         
-        // std::vector<std::uint8_t> decodeBuf;
-        // std::string decode_str = base64_decode(encode_str);
-        // decodeBuf.assign(decode_str.begin(),decode_str.end());
-        // std::cout << "decode bug size:" << decodeBuf.size() << std::endl;
+        // std::string decoded_string = base64_decode(encode_str);
+        // std::vector<uchar> data(decoded_string.begin(), decoded_string.end());
+        // cv::Mat decodeImg = cv::imdecode(data, cv::IMREAD_UNCHANGED);
+        // cv::imwrite("/home/li/d.jpg", decodeImg);
 
         usleep(1e5);
     }
 
     //report fault code
-    uint32_t code = 1;
     while (1)
     {
         std::cout << "SendReport response." << std::endl;
@@ -91,7 +93,6 @@ int main(int argc,char** argv){
         wf.SendResponse(json);
         
         sleep(1);
-        code++;
     }   
 
     std::cout << "exit..." << std::endl;
